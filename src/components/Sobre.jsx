4495527b7a52db05732a32fb6c6e79cf11ac634f
Ronaldo Vieira <ronaldovieira.lira@gmail.com>
2022-02-16 01:25:18 -0300

    css da area sobre, troca da imagem so sobre e modificação no texto do sobre

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -1,17 +1,19 @@
 import React from "react";
+import "../css/Sobre.css";
+import ImgSobre from "../imagens/encurta.svg";
 
 export default function Sobre() {
     return (
-        <div>
-            <div>
+        <div id="containerSobre">
+
+            <div id="textSobre">
                 <h1>Encurtador de URL</h1>
                 <p>
-                    Encurtador de URL Blinders é uma ferramenta poderosa para encurtar link. Use o nosso Encurtador de Link para diminuir uma URL longa e gerar um link curto.
+                    Encurtador de URL Blinders é uma ferramenta poderosa para encurtar link. Use o nosso Encurtador de link para diminuir uma URL longa e gerar uma nova URL curta. Cole a URL longa na caixa de texto e clique no botão Encurtar.
                 </p>
             </div>
-            <div>
-                <img src="" alt="" />
-            </div>
+            
+            <img src={ImgSobre} alt="sobre" id="imgSobre"/>
 
         </div>
     )

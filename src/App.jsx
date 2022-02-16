da82f1481823dd161a4ed3832548f396a3e9f3cb
Ronaldo Vieira <ronaldovieira.lira@gmail.com>
2022-02-16 00:55:37 -0300

    criação da pasta imagens e do arquivo App

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+import ReceptorURL from "./components/ReceptosURL";
+import Sobre from "./components/Sobre";
+
+
+export default function App() {
+    return (
+        <>
+            <Header />
+            <Sobre />
+            <ReceptorURL />
+        </>
+    )
+};

715c03ffea41eae6babc0324e7756af4d8cad068
Ronaldo Vieira <ronaldovieira.lira@gmail.com>
2022-02-16 01:59:09 -0300

    css Receptor

--- src/components/ReceptosURL.jsx
+++ src/components/ReceptosURL.jsx
@@ -4,7 +4,7 @@ import "../css/Receptor.css";
 export default function ReceptorURL() {
     return(
         <div id="caixaReceptor">
-            <input type="text" />
+            <input type="text" placeholder="Cole o link aqui! "/>
             <button type="button">ENCURTAR</button>
         </div>
     )

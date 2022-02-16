e5238d354e1894d9dc7be9bc56871950cb8e8624
Ronaldo Vieira <ronaldovieira.lira@gmail.com>
2022-02-16 18:16:56 -0300

    criacao do componente Motivo e novas imagens

---
+++ src/components/Motivos.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Motivos() {
+    return(
+        <div className="caixaMotivo">
+            <img src="" alt=""/>
+            <div className="areaTextMotivo">
+                <h1>TITULO</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor eligendi repellendus cupiditate commodi aliquid?</p>
+            </div>
+        </div>
+    )
+}

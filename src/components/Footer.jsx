b39ed7d667df5b168aad153fb500acaefc7a6d3f
Ronaldo Vieira <ronaldovieira.lira@gmail.com>
2022-02-16 23:36:09 -0300

    redução do size do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <footer>
             <h2> 
-                <strong id="copy">&copy; 2022</strong> Created By <strong id="eu"> <a href="https://ronaldo-vieira-lira.github.io/bioInsta/" target="_blank"> Ronaldo Vieira </a> </strong> | All Rights Reserved!
+                <strong id="copy">&copy;2022</strong> Created By <strong id="eu"> <a href="https://ronaldo-vieira-lira.github.io/bioInsta/" target="_blank"> Ronaldo Vieira </a> </strong> | All Rights Reserved!
             </h2>
         </footer>
     )

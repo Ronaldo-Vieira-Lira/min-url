18c3466559ab1d4aede65eae776f0d05d616d876
Ronaldo Vieira <ronaldovieira.lira@gmail.com>
2022-02-16 22:20:54 -0300

    ajustando a largura e a altura das imagens

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
+import Motivos from "./components/Motivos";
 import ReceptorURL from "./components/ReceptosURL";
 import Sobre from "./components/Sobre";
+import "./css/App.css";
 
+import Free from "./imagens/free.svg";
+import Link from "./imagens/link.svg";
+import Respo from "./imagens/responsivo.svg";
+import Cadeado from "./imagens/cadeado.svg";
 
 export default function App() {
     return (
@@ -10,6 +16,24 @@ export default function App() {
             <Header />
             <Sobre />
             <ReceptorURL />
+            <div id="containerMotivos">
+                <Motivos 
+                    img={Link} 
+                    title="Link" 
+                    descricao="Encurte links de qualquer tamanho com o Encurtador"/>
+                <Motivos 
+                    img={Free} 
+                    title="Gratuito"
+                    descricao="Crie quantos links encurtados quiser e compartilhe"/>
+                <Motivos 
+                    img={Respo} 
+                    title="Responsivo"
+                    descricao="Compatível com desktop, tablets e com dispositivos móveis"/>
+                <Motivos 
+                    img={Cadeado} 
+                    title="Seguro"
+                    descricao="Usamos protocolo https com criptografia de dados"/>
+            </div>
         </>
     )
 };
